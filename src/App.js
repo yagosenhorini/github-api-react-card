473b69b9f1c35b4c51b6059b63d01038626995c4
yagosenhorini <yagosenhorini30@gmail.com>
2018-04-10 01:08:12 -0300

    Initialize react github api

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import React, { Component } from 'react';
+import './App.css';
+
+const Card = (props) =>{
+  return (
+    <div style={{margin: '1em'}}>
+      <img src="http://placehold.it/75" />
+      <div style={{display: 'inline-block', marginLeft: 10}}>
+        <div style={{fontSize: '1.25em', fontWeight: 'bold'}}>Name Here</div>
+        <div>Company name here...</div>
+        </div>
+    </div>
+  );
+};
+
+
+class App extends Component {
+  
+}
+
+export default App;
